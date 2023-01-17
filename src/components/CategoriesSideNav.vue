907a8e09fc4b11245f1a8b7e69641e6f811690e1
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-01-17 23:23:24 +0500

    Categories components

---
+++ src/components/CategoriesSideNav.vue
@@ -0,0 +1,34 @@
+<template>
+  <BaseSideNav :sidenav-extra-classes="`app-sidenav open`">
+    <template #sidenav-content>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/">Главная(Счёт)</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/categories">Категории</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/history">История</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/planning">Планирование</router-link>
+      </li>
+      <li>
+        <router-link class="waves-effect waves-orange pointer" to="/record/new">Новая запись</router-link>
+      </li>
+    </template>
+  </BaseSideNav>
+</template>
+
+<script>
+import BaseSideNav from "@/components/UI/BaseSideNav.vue";
+
+export default {
+  name: "CategoriesSideNav",
+  components: {BaseSideNav}
+}
+</script>
+
+<style scoped>
+
+</style>
