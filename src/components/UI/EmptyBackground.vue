3d8bb740eec5f9540d5d0adefcee750fbb56e9da
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-01-17 23:23:50 +0500

    Empty component

---
+++ src/components/UI/EmptyBackground.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grey darken-1 empty-layout">
+    <slot name="layout-content"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmptyBackground"
+}
+</script>
+
+<style scoped>
+
+</style>
