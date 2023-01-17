78444435c7df5ec442dc435f88904b357e978b73
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-01-17 23:25:31 +0500

    History detail view

---
+++ src/views/HistoryDetail.vue
@@ -0,0 +1,33 @@
+<template>
+  <ClassicLayout>
+    <div>
+      <div class="breadcrumb-wrap">
+        <router-link class="waves-effect waves-orange pointer" to="/history">История</router-link>
+        <a class="breadcrumb">
+          Расход
+        </a>
+      </div>
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card red">
+            <div class="card-content white-text">
+              <p>Описание:</p>
+              <p>Сумма:</p>
+              <p>Категория:</p>
+              <small>12.12.12</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </ClassicLayout>
+</template>
+
+<script>
+import ClassicLayout from "@/layouts/ClassicLayout.vue";
+
+export default {
+  name: "HistoryDetail",
+  components: {ClassicLayout}
+}
+</script>
