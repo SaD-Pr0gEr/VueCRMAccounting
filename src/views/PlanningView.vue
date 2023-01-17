66418ba472cbfe9e9fb133c460d540a9aa259b10
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-01-17 23:25:15 +0500

    Planning view

---
+++ src/views/PlanningView.vue
@@ -0,0 +1,33 @@
+<template>
+  <ClassicLayout>
+    <div>
+      <div class="page-title">
+        <h3>Планирование</h3>
+        <h4>12 212</h4>
+      </div>
+      <section>
+        <div>
+          <p>
+            <strong>Девушка:</strong>
+            12 122 из 14 0000
+          </p>
+          <div class="progress">
+            <div
+                class="determinate green"
+                style="width:40%"
+            ></div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </ClassicLayout>
+</template>
+
+<script>
+import ClassicLayout from "@/layouts/ClassicLayout.vue";
+
+export default {
+  name: "PlanningView",
+  components: {ClassicLayout}
+}
+</script>
