f183b8c6d2ffc46e2f4d3774731a95853031055b
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-01-17 23:23:12 +0500

    Basic components

---
+++ src/components/UI/BaseNavbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar orange lighten-1">
+    <div class="nav-wrapper">
+      <slot name="base-navbar-content"></slot>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "BaseNavbar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UI/BaseSideNav.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul :class="`sidenav ${sidenavExtraClasses}`">
+    <slot name="sidenav-content"></slot>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "BaseSideNavbar",
+  props: ["sidenavExtraClasses"]
+}
+</script>
+
+<style scoped>
+
+</style>
