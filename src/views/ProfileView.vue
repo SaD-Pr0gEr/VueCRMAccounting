7301fcf4c0d0baeb538fb8ca543aebb5313e562d
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-01-17 23:25:08 +0500

    Profile view

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,33 @@
+<template>
+  <ClassicLayout>
+    <div>
+      <div class="page-title">
+        <h3>Профиль</h3>
+      </div>
+      <form class="form">
+        <div class="input-field">
+          <input
+              id="description"
+              type="text"
+          >
+          <label for="description">Имя</label>
+          <span
+              class="helper-text invalid">name</span>
+        </div>
+        <button class="btn waves-effect waves-light" type="submit">
+          Обновить
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </ClassicLayout>
+</template>
+
+<script>
+import ClassicLayout from "@/layouts/ClassicLayout.vue";
+
+export default {
+  name: "ProfileView",
+  components: {ClassicLayout}
+}
+</script>
