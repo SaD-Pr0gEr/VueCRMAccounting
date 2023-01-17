b4b663f586c2dcb835659b28f7dc900dc2c85081
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-01-17 23:24:05 +0500

    Classic layout

---
+++ src/layouts/ClassicLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-main-layout">
+    <BaseNavbar>
+      <template #base-navbar-content>
+        <CategoriesNavbar/>
+      </template>
+    </BaseNavbar>
+    <CategoriesSideNav/>
+    <main class="app-content">
+      <div class="app-page">
+        <slot></slot>
+      </div>
+    </main>
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseNavbar from "@/components/UI/BaseNavbar.vue";
+import CategoriesNavbar from "@/components/CategoriesNavbar.vue";
+import CategoriesSideNav from "@/components/CategoriesSideNav.vue";
+
+export default {
+  name: "ClassicLayout",
+  components: {CategoriesSideNav, CategoriesNavbar, BaseNavbar}
+}
+</script>
+
+<style scoped>
+
+</style>
