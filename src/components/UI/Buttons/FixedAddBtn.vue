432220554fb5af9dd6d1fb6c25bb296a3526f271
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-01-18 16:44:49 +0500

    fixed btn component

---
+++ src/components/UI/Buttons/FixedAddBtn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="fixed-action-btn">
+    <router-link :to="{name: 'newRecord'}" class="btn-floating btn-large blue">
+      <i class="large material-icons">add</i>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FixedAddBtn"
+}
+</script>
+
+<style scoped>
+
+</style>
